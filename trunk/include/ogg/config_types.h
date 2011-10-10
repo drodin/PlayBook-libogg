23a6f27d9b453471d800f4cf87d6d161555d7219
Dmitry Rodin <rodin.dmitry@gmail.com>
2011-10-10 21:15:44 +0700

    set types for QNX

---
+++ trunk/include/ogg/config_types.h
@@ -0,0 +1,10 @@
+#ifndef __CONFIG_TYPES_H__
+#define __CONFIG_TYPES_H__
+
+typedef _int16 ogg_int16_t;
+typedef _uint16 ogg_uint16_t;
+typedef _int32 ogg_int32_t;
+typedef _uint32 ogg_uint32_t;
+typedef _int64 ogg_int64_t;
+
+#endif
